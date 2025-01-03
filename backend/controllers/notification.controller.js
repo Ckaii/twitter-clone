5b3e16c38bd46cff68882806fda915cb41489196
CKaiii_ <tanwenkai2004@gmail.com>
2025-01-03 16:30:53 +0800

    Like Post Mutation Added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,13 +1,32 @@
-import express from "express";
-import { protectRoute } from "../middleware/protectRoute.js";
-import {
-  deleteNotifications,
-  getNotifications,
-} from "../controllers/notification.controller.js";
+import Notification from "../models/notification.model.js";
 
-const router = express.Router();
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
 
-router.get("/", protectRoute, getNotifications);
-router.delete("/", protectRoute, deleteNotifications);
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: "from",
+      select: "username profileImg",
+    });
 
-export default router;
+    await Notification.updateMany({ to: userId }, { read: true });
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.log("Error in getNotifications function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: "Notifications deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteNotifications function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
